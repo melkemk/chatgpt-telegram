c9f5ea8bf5c82fb6a51303f8e05a16f7c64ac42a
melkemk <melkemk503@gmail.com>
2023-03-08 10:56:15 +0300

    deleted unusual files

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import os
-
+from datetime import datetime
+from googletrans import Translator
 import openai
 import telegram
 from dotenv import load_dotenv
 from telegram.ext import Filters, MessageHandler, Updater
-
-totalMessage = 0
+from easygoogletranslate import EasyGoogleTranslate
+users_data = []
+user = {}
 load_dotenv()
-
+totalMessages = 0
 openai.api_key = os.getenv("OPENAI_API_KEY")
-model_engine = (
-    "text-davinci-002"  # or "text-davinci-003" for a more capable (and expensive) model
-)
+model_engine = "text-davinci-002"
 
-# define a function to generate a response using OpenAI
+
 def generate_response(prompt):
+
     response = openai.Completion.create(
         engine=model_engine,
         prompt=prompt,
@@ -26,13 +27,52 @@ def generate_response(prompt):
     return message
 
 
+def check_total(update):
+
+    user_id = update.message.from_user.id
+    global users_data
+    global user
+    found = False
+    for row in users_data:
+        if row["id"] == user_id:
+            found = True
+            row["count"] += 1
+            if row["count"] > 25:
+                if row["count"] == 26:
+                    allowed_time = datetime.now()
+                    allowed_time = allowed_time.replace(hour=allowed_time.hour % 24 + 5)
+                    row["ban_time"] = allowed_time
+                    return True
+                if isinstance(row["ban_time"], datetime):
+                    if row["ban_time"] < datetime.now():
+                        row["count"] = 0
+                        return True
+                update.message.reply_text("please wait ")
+            else:
+                return True
+    if not found:
+        user = {"id": user_id, "count": 0, "ban_time": datetime.now()}
+        users_data.append(user)
+        return True
+def toEnglish(text):
+    translator = EasyGoogleTranslate(
+        target_language='en',
+        timeout=10
+    )
+    return(translator.translate(text))
+def toAmharic(text):
+    translator = EasyGoogleTranslate(
+        target_language='am',
+        timeout=10
+    )
+    return(translator.translate(text))
+
 def handle_message(update, context):
-    user_message = update.message.text
-    totalMessage += 1
-    print(totalMessage)
-    prompt = f"User: {user_message}\nAI:"
-    ai_response = generate_response(prompt)
-    update.message.reply_text(totalMessage)
+    if check_total(update):
+        user_message = toEnglish(update.message.text)
+        prompt = f"User: {user_message}\nAI:"
+        ai_response = generate_response(prompt)
+        update.message.reply_text(toAmharic(ai_response))
 
 
 bot = telegram.Bot(token=os.getenv("env"))
@@ -41,5 +81,4 @@ updater = Updater(bot.token, use_context=True)
 dispatcher = updater.dispatcher
 
 dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
-
 updater.start_polling()
